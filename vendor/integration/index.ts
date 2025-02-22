8e428eeced9c68dc8e20a0a99d42f2b698e7cc28
k15r <git@riddance.k15r.xyz>
2025-02-22 23:57:59 +0100

    Expose astrowind:config types.

--- vendor/integration/index.ts
+++ vendor/integration/index.ts
@@ -22,7 +22,7 @@ export default ({ config: _themeConfig = 'src/config.yaml' } = {}): AstroIntegra
       }) => {
         const buildLogger = logger.fork('astrowind');
 
-        const virtualModuleId = 'astrowind:config';
+        const virtualModuleId = '\0#astrowind:config';
         const resolvedVirtualModuleId = '\0' + virtualModuleId;
 
         const rawJsonConfig = (await loadConfig(_themeConfig)) as Config;
