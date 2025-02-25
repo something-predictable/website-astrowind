04a12c32c740c42a9aee288f1f810ea6ce442b89
k15r <git@riddance.k15r.xyz>
2025-02-25 22:30:57 +0100

    Expose config types.

--- vendor/integration/types.d.ts
+++ vendor/integration/types.d.ts
@@ -1,10 +1,88 @@
 declare module '#astrowind:config' {
-  import type { SiteConfig, I18NConfig, MetaDataConfig, AppBlogConfig, UIConfig, AnalyticsConfig } from './config';
-
   export const SITE: SiteConfig;
   export const I18N: I18NConfig;
   export const METADATA: MetaDataConfig;
   export const APP_BLOG: AppBlogConfig;
   export const UI: UIConfig;
   export const ANALYTICS: AnalyticsConfig;
+}
+
+export type Config = {
+  site?: SiteConfig;
+  metadata?: MetaDataConfig;
+  i18n?: I18NConfig;
+  apps?: {
+    blog?: AppBlogConfig;
+  };
+  ui?: unknown;
+  analytics?: unknown;
+};
+
+export interface SiteConfig {
+  name: string;
+  site?: string;
+  base?: string;
+  trailingSlash?: boolean;
+  googleSiteVerificationId?: string;
+}
+export interface MetaDataConfig extends Omit<MetaData, 'title'> {
+  title?: {
+    default: string;
+    template: string;
+  };
+}
+export interface I18NConfig {
+  language: string;
+  textDirection: string;
+  dateFormatter?: Intl.DateTimeFormat;
+}
+export interface AppBlogConfig {
+  isEnabled: boolean;
+  postsPerPage: number;
+  isRelatedPostsEnabled: boolean;
+  relatedPostsCount: number;
+  post: {
+    isEnabled: boolean;
+    permalink: string;
+    robots: {
+      index: boolean;
+      follow: boolean;
+    };
+  };
+  list: {
+    isEnabled: boolean;
+    pathname: string;
+    robots: {
+      index: boolean;
+      follow: boolean;
+    };
+  };
+  category: {
+    isEnabled: boolean;
+    pathname: string;
+    robots: {
+      index: boolean;
+      follow: boolean;
+    };
+  };
+  tag: {
+    isEnabled: boolean;
+    pathname: string;
+    robots: {
+      index: boolean;
+      follow: boolean;
+    };
+  };
+}
+export interface AnalyticsConfig {
+  vendors: {
+    googleAnalytics: {
+      id?: string;
+      partytown?: boolean;
+    };
+  };
+}
+
+export interface UIConfig {
+  theme: string;
 }
